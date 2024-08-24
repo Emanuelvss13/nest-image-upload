9583fd1a41b75d2b641fe738446ca80e1fb0de3d
Emanuelvss13 <emanuelsouza2002@gmail.com>
2024-08-24 08:55:37 -0300

    feat: user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,9 +1,27 @@
+import * as bcrypt from 'bcrypt';
+import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
 import { BaseEntity } from '../../global/entities/base.entity';
 
+@Entity()
 export class User extends BaseEntity {
+  @Column()
   username: string;
 
+  @Column()
   email: string;
 
+  @Column()
   password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    if (this.password) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+  }
+
+  async validatePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
 }
