879e4b84267c309370b417af2f80c92386a4df1a
Emanuelvss13 <emanuelsouza2002@gmail.com>
2024-08-24 14:51:44 -0300

    feat: multer upload controller

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,15 +1,53 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ImagesService } from './images.service';
-import { CreateImageDto } from './dto/create-image.dto';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { randomUUID } from 'crypto';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { JwtAuthGuard } from '../auth/auth.guard';
+import { CurrentUser } from '../auth/current-user.decorator';
+import { User } from '../user/entities/user.entity';
 import { UpdateImageDto } from './dto/update-image.dto';
+import { ImagesService } from './images.service';
 
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
-  @Post()
-  create(@Body() createImageDto: CreateImageDto) {
-    return this.imagesService.create(createImageDto);
+  @Post('upload')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: 'dist/src/tmp',
+        filename: (req, file, cb) => {
+          const filename = `${randomUUID()}-${Date.now()}-image`;
+          const extension = extname(file.originalname);
+
+          cb(null, `${filename}${extension}`);
+        },
+      }),
+    }),
+  )
+  upload(@UploadedFile() file: Express.Multer.File, @CurrentUser() user: User) {
+    const fileType = file.mimetype.split('/')[0];
+
+    if (fileType !== 'image') {
+      throw new BadRequestException('Envie apenas imagens!');
+    }
+
+    return this.imagesService.upload(file.path, user.id);
   }
 
   @Get()
